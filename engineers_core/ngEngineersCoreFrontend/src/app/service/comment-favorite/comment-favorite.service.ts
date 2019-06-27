e7363b4518f0646aa51fdd3c822a845c92727d2a
Ryo Saito <ryosaito@RyonoMacBook-Pro.local>
2019-06-27 19:31:48 +0900

    COMMENT_FAVORITEのURLパターン修正

--- engineers_core/ngEngineersCoreFrontend/src/app/service/comment-favorite/comment-favorite.service.ts
+++ engineers_core/ngEngineersCoreFrontend/src/app/service/comment-favorite/comment-favorite.service.ts
@@ -1,20 +1,18 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {SigninService} from '../signin/signin.service';
 import {Observable} from 'rxjs';
+
+import {SigninService} from '../signin/signin.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentFavoriteService {
 
-  host = 'http://127.0.0.1:8000/';
-  commentFavoritesUrl = 'api/commentfavorites/';
-  commentFavoritesAPIUrl = this.host + this.commentFavoritesUrl;
-  accountNameCommentFavoritesUrl = 'api/commentfavorites/?account_name=';
-  accountNameCommentFavoritesAPIUrl = this.host + this.accountNameCommentFavoritesUrl;
-  commentFavoriteUrl = 'api/commentfavorite/';
-  commentFavoriteAPIUrl = this.host + this.commentFavoriteUrl;
+  HOST = 'http://127.0.0.1:8000/';
+  COMMENT_FAVORITES_API_URL = this.HOST + 'api/comment/favorites/';
+  ACCOUNT_NAME_COMMENT_FAVORITES_API_URL = this.HOST + 'api/comment/favorites/?account_name=';
+  COMMENT_FAVORITE_API_URL = this.HOST + 'api/comment/favorite/';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -27,18 +25,18 @@ export class CommentFavoriteService {
   ) { }
 
   getCommentFavorite(userId: number, commentId: number): Observable<any> {
-    const url = this.commentFavoritesAPIUrl + '?user_id=' + userId + '&comment_id=' + commentId;
+    const url = this.COMMENT_FAVORITES_API_URL + '?user_id=' + userId + '&comment_id=' + commentId;
     console.log('getCommentFavorite。url: ' + url);
     return this.http.get<any>(url, this.httpOptions);
   }
 
   getUserCommentFavorites(userId: number): Observable<any> {
-    const url = this.commentFavoritesAPIUrl + '?user_id=' + userId;
+    const url = this.COMMENT_FAVORITES_API_URL + '?user_id=' + userId;
     return this.http.get<any>(url, this.httpOptions);
   }
 
   getCommentFavoritesByAccountName(accountName: string): Observable<any> {
-    const url = this.accountNameCommentFavoritesAPIUrl + accountName;
+    const url = this.ACCOUNT_NAME_COMMENT_FAVORITES_API_URL + accountName;
     return this.http.get<any>(url, this.httpOptions);
   }
 
@@ -47,11 +45,11 @@ export class CommentFavoriteService {
       user: userId,
       comment: commentId,
     };
-    return this.http.post<any>(this.commentFavoritesAPIUrl, body, this.httpOptions);
+    return this.http.post<any>(this.COMMENT_FAVORITES_API_URL, body, this.httpOptions);
   }
 
   deleteCommentFavorite(commentFavoriteId: number): Observable<any> {
-    const url = this.commentFavoriteAPIUrl + commentFavoriteId + '/';
+    const url = this.COMMENT_FAVORITE_API_URL + commentFavoriteId + '/';
     const jwtHeader = this.signinService.createJwtHeaderFromLocalStorage();
     return this.http.delete<any>(url, {headers: jwtHeader});
   }
