126d1013191b8ac3ed55220fe0a92d478e14d1d2
Ryo Saito <ryosaito@RyonoMacBook-Pro.local>
2019-07-01 01:54:53 +0900

    本の詳細画面で返信表示,コメントメニュー実装

--- engineers_core/ngEngineersCoreFrontend/src/app/component/book-detail/book-detail.component.ts
+++ engineers_core/ngEngineersCoreFrontend/src/app/component/book-detail/book-detail.component.ts
@@ -3,7 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {BsModalRef, BsModalService} from 'ngx-bootstrap';
 import {AuthGuard} from '../../guard/auth.guard';
 import {NgForm} from '@angular/forms';
-import {faCommentDots, faHeart, faBook} from '@fortawesome/free-solid-svg-icons';
+import {faCommentDots, faHeart, faBook, faCaretDown, faExclamationTriangle} from '@fortawesome/free-solid-svg-icons';
 
 import {AppComponent} from '../../app.component';
 import {Book} from '../../dto/book/book';
@@ -15,6 +15,8 @@ import {InterestedBookService} from '../../service/interested-book/interested-bo
 import {BookAuthorService} from '../../service/book-author/book-author.service';
 import {CommentFavoriteService} from '../../service/comment-favorite/comment-favorite.service';
 import {UserService} from '../../service/user/user.service';
+import {BookCommentReplyService} from '../../service/book-comment-reply/book-comment-reply.service';
+import {BookCommentReplyFavoriteService} from '../../service/book-comment-reply-favorite/book-comment-reply-favorite.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -23,12 +25,15 @@ import {UserService} from '../../service/user/user.service';
 })
 export class BookDetailComponent implements OnInit {
 
-  pageFound = true;
-  bookId = Number(this.route.snapshot.paramMap.get('id'));
-  @Input() book: Book;
   faHeart = faHeart;
   faCommentDots = faCommentDots;
   faBook = faBook;
+  faCaretDown = faCaretDown;
+  faExclamationTriangle = faExclamationTriangle;
+
+  pageFound = true;
+  bookId = Number(this.route.snapshot.paramMap.get('id'));
+  @Input() book: Book;
   userId: number;
   accountName: string;
   bookComments: BookComment[];
@@ -42,15 +47,17 @@ export class BookDetailComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private authGuard: AuthGuard,
+    private modalService: BsModalService,
     private appComponent: AppComponent,
+    private signinService: SigninService,
+    private userService: UserService,
     private bookService: BookService,
     private bookAuthorService: BookAuthorService,
     private bookCommentService: BookCommentService,
     private interestedBookService: InterestedBookService,
     private commentFavoriteService: CommentFavoriteService,
-    private signinService: SigninService,
-    private userService: UserService,
-    private modalService: BsModalService
+    private bookCommentReplyService: BookCommentReplyService,
+    private bookCommentReplyFavoriteService: BookCommentReplyFavoriteService,
   ) {
   }
 
@@ -243,6 +250,30 @@ export class BookDetailComponent implements OnInit {
     });
   }
 
+  deleteComment(commentId: number, index: number): void {
+    if (!this.authGuard.canActivate()) {
+      return;
+    }
+    this.bookCommentService.deleteComment(commentId).subscribe(res => {
+      this.bookComments.splice(index, 1);
+      this.bookCommentCount -= 1;
+    }, error => {
+      console.error('deleteCommentでエラー: ', error);
+    });
+  }
+
+  reportComment(commentId: number, reasonCode: string, index: number): void {
+    if (!this.authGuard.canActivate()) {
+      return;
+    }
+    this.bookCommentService.reportComment(this.appComponent.userId, commentId, reasonCode).subscribe(res => {
+      // 報告済みマークつける
+      this.bookComments[index].reportUserIds.push(this.appComponent.userId);
+    }, error => {
+      console.error('reportCommentでエラー: ', error);
+    });
+  }
+
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
@@ -253,5 +284,108 @@ export class BookDetailComponent implements OnInit {
     const month = ('0' + (now.getMonth() + 1)).slice(-2);
     const day = ('0' + now.getDate()).slice(-2);
     this.today = year + '-' + month + '-' + day;
+  }
+
+  reverseRepliesDisplayState(shelfCommentId: number, commentIndex: number, page: number): void {
+    // 表示中に呼ばれた場合には非表示、非表示中に呼ばれた場合には表示
+    this.bookComments[commentIndex].displayedReply = !this.bookComments[commentIndex].displayedReply;
+    this.bookCommentReplyService.getCommentRepliesPagingByCommentId(shelfCommentId, page).subscribe(data => {
+      if (data.count > 0) {
+        this.bookComments[commentIndex].commentReplies = this.bookCommentReplyService.convertBookCommentReplies(data.results);
+      }
+    }, error => {
+      console.error('displayRepliesでエラー: ', error);
+    });
+  }
+
+  registerReply(f: NgForm, commentId: number, commentIndex: number) {
+    if (!this.authGuard.canActivate()) {
+      return;
+    }
+    const comment = f.value.comment;
+    // const tweetFlag = f.value.tweetFlag;
+    const tweetFlag = false;
+    this.bookCommentReplyService.registerCommentReply(this.appComponent.userId, commentId, comment, tweetFlag).subscribe(res => {
+      this.bookCommentReplyService.getCommentReply(res.id).subscribe(response => {
+        if (response.id !== undefined) {
+          const shelfCommentReply = this.bookCommentReplyService.convertBookCommentReply(response);
+          this.bookComments[commentIndex].commentReplies.unshift(shelfCommentReply);
+          this.bookComments[commentIndex].replyUserCount += 1;
+          f.reset();
+        }
+      });
+    });
+  }
+
+  deleteReply(commentReplyId: number, commentIndex: number, replyIndex: number): void {
+    if (!this.authGuard.canActivate()) {
+      return;
+    }
+    this.bookCommentReplyService.deleteCommentReply(commentReplyId).subscribe(res => {
+      this.bookComments[commentIndex].commentReplies.splice(replyIndex, 1);
+      this.bookComments[commentIndex].replyUserCount -= 1;
+    }, error => {
+      console.error('deleteReplyでエラー: ', error);
+    });
+  }
+
+  reportReply(commentReplyId: number, reasonCode: string, commentIndex: number, replyIndex: number): void {
+    if (!this.authGuard.canActivate()) {
+      return;
+    }
+    this.bookCommentReplyService.reportReply(this.appComponent.userId, commentReplyId, reasonCode).subscribe(res => {
+      // 報告済みマークつける
+      this.bookComments[commentIndex].commentReplies[replyIndex].reportUserIds.push(this.appComponent.userId);
+    }, error => {
+      console.error('reportReplyでエラー: ', error);
+    });
+  }
+
+  replyFavorite(commentReplyId: number, commentIndex: number, replyIndex: number): void {
+    if (!this.authGuard.canActivate()) {
+      return;
+    }
+    const loggedInUserId = this.appComponent.userId;
+    this.bookCommentReplyFavoriteService.getReplyFavorite(loggedInUserId, commentReplyId).subscribe(data => {
+      // まだデータが存在しない場合は作成する。
+      if (data === null || data === undefined || data.count === 0) {
+        this.bookCommentReplyFavoriteService.registerReplyFavorite(loggedInUserId, commentReplyId).subscribe(
+          (res) => {
+            this.bookComments[commentIndex].commentReplies[replyIndex].favoriteUserIds.push(loggedInUserId);
+            this.bookComments[commentIndex].commentReplies[replyIndex].favoriteUserCount += 1;
+          },
+          (error) => {
+            console.error('replyFavoriteでerror: ' + JSON.stringify(error));
+          }
+        );
+      } else {
+        console.error('replyFavoriteが呼ばれるのおかしい。loggedInUserId: ' + loggedInUserId, 'commentReplyId: ' + commentReplyId);
+      }
+    });
+  }
+
+  notReplyFavorite(commentReplyId: number, commentIndex: number, replyIndex: number): void {
+    if (!this.authGuard.canActivate()) {
+      return;
+    }
+    const loggedInUserId = this.appComponent.userId;
+    this.bookCommentReplyFavoriteService.getReplyFavorite(loggedInUserId, commentReplyId).subscribe(data => {
+      // データがある場合は削除する。
+      if (data !== null && data !== undefined && data.count !== 0) {
+        const favoriteId = data.results[0].id;
+        this.bookCommentReplyFavoriteService.deleteReplyFavorite(favoriteId).subscribe(
+          (res) => {
+            const userIdIndex = this.bookComments[commentIndex].commentReplies[replyIndex].favoriteUserIds.indexOf(loggedInUserId);
+            this.bookComments[commentIndex].commentReplies[replyIndex].favoriteUserIds.splice(userIdIndex, 1);
+            this.bookComments[commentIndex].commentReplies[replyIndex].favoriteUserCount -= 1;
+          },
+          (error) => {
+            console.log('notReplyFavoriteでerror: ' + error);
+          }
+        );
+      } else {
+        console.error('まだデータが存在しない場合はメソッド呼ばれるのおかしい。commentReplyId: ' + commentReplyId);
+      }
+    });
   }
 }
